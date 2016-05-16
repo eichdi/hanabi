e5b3cf199ad77e186b7328a0dec05e62267b726b
eichdi <eichdi96@gmail.com>
2016-05-16 18:13:04 +0300

    echo tellegram

--- hanabi/Program.cs
+++ hanabi/Program.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using hanabi.Controller;
-
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using File = System.IO.File;
+using System.IO;
 namespace hanabi
 {
    
@@ -13,21 +16,74 @@ namespace hanabi
     {
         static void Main(string[] args)
         {
-            int level = 2;
-            Command com = new Command(level);
-            string result;
+            var Bot = new Api("225115203:AAH_vGJDopLajGzNSK16YkQLjGBCZzVUT10");
 
-            string scommand = Console.ReadLine();
-            while (scommand != null)
+            Run(Bot).Wait();
+        }
+
+        static async Task Run(Api Bot)
+        {
+
+
+            var me = await Bot.GetMe();
+
+            Console.WriteLine("Hello my name is {0}", me.Username);
+
+            var offset = 0;
+
+            while (true)
             {
-                com.DoFunc(scommand);
-                result = com.GetResultGame();
-                if (result != null)
+                var updates = await Bot.GetUpdates(offset);
+
+                foreach (var update in updates)
                 {
-                    Console.WriteLine(result);
+                    if (update.Message.Type == MessageType.TextMessage)
+                    {
+                        await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
+                        //await Task.Delay(2000);
+                        var t = await Bot.SendTextMessage(update.Message.Chat.Id, update.Message.Text);
+                        Console.WriteLine("Echo Message: {0}", update.Message.Text);
+                    }
+
+                    if (update.Message.Type == MessageType.PhotoMessage)
+                    {
+                        var file = await Bot.GetFile(update.Message.Photo.LastOrDefault().FileId);
+
+                        Console.WriteLine("Received Photo: {0}", file.FilePath);
+
+                        var filename = file.FileId+"."+file.FilePath.Split('.').Last();
+
+                        using (var profileImageStream = File.Open(filename, FileMode.Create))
+                        {
+                            await file.FileStream.CopyToAsync(profileImageStream);
+                        }
+                    }
+
+                    offset = update.Id + 1;
                 }
-                scommand = Console.ReadLine();
+
+                //await Task.Delay(1000);
             }
         }
+        //static void Main(string[] args)
+        //{
+
+           
+        //    int level = 2;
+        //    Command com = new Command(level);
+        //    string result;
+        //    var bot = new Api("your token");
+        //    //string scommand = Console.ReadLine();
+        //    //while (scommand != null)
+        //    //{
+        //    //    com.DoFunc(scommand);
+        //    //    result = com.GetResultGame();
+        //    //    if (result != null)
+        //    //    {
+        //    //        Console.WriteLine(result);
+        //    //    }
+        //    //    scommand = Console.ReadLine();
+        //    //}
+        //}
     }
 }
