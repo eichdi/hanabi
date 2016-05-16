b209b8c9388df92de8f0f7377e95f8a49f29311e
eichdi <eichdi96@gmail.com>
2016-05-16 14:01:23 +0300

    refactor

---
+++ hanabi/Controller/Command.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hanabi.GameLogic;
+
+namespace hanabi.Controller
+{
+    class Command
+    {
+        public Game game;
+        private int turn;
+        private int level;
+        public bool State
+        {
+            get
+            {
+                return game != null && game.State;
+            }
+        }
+        public void StartGame(string[] card)
+        {
+            turn = 0;
+            Player player1 = new Player(null);
+            Player player2 = new Player(null);
+            PackOfCard pack = new PackOfCard(card);
+            game = new Game(player1, player2, pack);
+            player1.game = game;
+            player2.game = game;
+        }
+
+
+        public void PlayCard(string sindex)
+        {
+            try
+            {
+                int index = int.Parse(sindex);
+                game.CurrentPlayer.PlayCard(index);
+            }
+            catch
+            {
+                throw new Exception("input exception");
+            }
+        }
+        public void DropCard(string sindex)
+        {
+            try
+            {
+                int index = int.Parse(sindex);
+                game.CurrentPlayer.Drop(index);
+            }
+            catch
+            {
+                throw new Exception("input exception");
+            }
+        }
+        public void TellColor(string color, string[] cards)
+        {
+            try
+            {
+                int[] index = new int[cards.Length];
+                for (int i = 0; i < cards.Length; i++)
+                {
+                    index[i] = int.Parse(cards[i]);
+                }
+                game.CurrentPlayer.TellColor(color, index);
+            }
+            catch
+            {
+                throw new Exception("input exception");
+            }
+        }
+        public void TellRank(string srank, string[] cards)
+        {
+            try
+            {
+                int rank = int.Parse(srank);
+                int[] index = new int[cards.Length];
+                for (int i = 0; i < cards.Length; i++)
+                {
+                    index[i] = int.Parse(cards[i]);
+                }
+                game.CurrentPlayer.TellRank(rank, index);
+            }
+            catch
+            {
+                throw new Exception("input exception");
+            }
+        }
+
+        public Command(int level)
+        {
+            this.level = level;
+        }
+
+
+        public bool DoFunc(string command)
+        {
+            if (true)
+            {
+                turn++;
+                if (command.Contains("Start new game with deck "))
+                {
+                    StartGame(Service.GetCards("Start new game with deck ", command));
+                    return true;
+                }
+                if (command.Contains("Play card "))
+                {
+                    PlayCard(Service.GetCard("Play card ", command));
+                    return true;
+                }
+                if (command.Contains("Tell rank "))
+                {
+                    string[] analize = command.Split(' ');
+                    TellRank(analize[2], Service.GetCards(("Tell rank " + analize[2] + " for cards "), command));
+                    return true;
+                }
+                if (command.Contains("Tell color "))
+                {
+                    string[] analize = command.Split(' ');
+                    TellColor(analize[2], Service.GetCards(("Tell color " + analize[2] + " for cards "), command));
+                    return true;
+                }
+                if (command.Contains("Drop card "))
+                {
+                    DropCard(Service.GetCard("Drop card ", command));
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string GetResultGame()
+        {
+            if (this.State == false)
+            {
+                if (level == 2)
+                {
+                    return "Turn: " + this.turn.ToString() + ", cards: " + game.GamedCards.ToString() + ", with risk: " + game.Risk.ToString();
+                }
+                else
+                {
+                    return "Turn: " + this.turn.ToString() + ", cards: " + game.GamedCards.ToString() + ", with risk: " + "0";
+                }
+            }
+            else return null;
+        }
+        public string GetResult(string command)
+        {
+            if (this.game != null)
+            {
+                string result = "";
+                result += "\n Turn: " + this.turn.ToString() + ", cards: " + game.GamedCards.ToString() + ", with risk: " + game.Risk.ToString() + ", state: " + this.State.ToString();
+                result += "\n" + "  Current player: " + this.game.GetCurrentPlayerCard();
+                result += "\n" + "                  " + this.game.GetRiskCurrentPlayerCard();
+                result += "\n" + "     Next player: " + this.game.GetNextPlayerCard();
+                result += "\n" + "                  " + this.game.GetRiskNextPlayerCard();
+                result += "\n" + "           Table: " + this.game.GetTableCard();
+                result += "\n" + command;
+                return result;
+            }
+            else
+            {
+                return command;
+            }
+        }
+
+
+
+    }
+}
---
+++ hanabi/Service.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hanabi.GameLogic;
+namespace hanabi
+{
+    class Service
+    {
+        protected static int id = 0;
+
+
+        public static int GetID
+        {
+            get
+            {
+                id++;
+                return id;
+            }
+        }
+        public static string ConvertColor(string color)
+        {
+            switch (color)
+            {
+                case "R":
+                    return Card.Red;
+                case "Y":
+                    return Card.Yellow;
+                case "B":
+                    return Card.Blue;
+                case "W":
+                    return Card.White;
+                case "G":
+                    return Card.Green;
+            }
+            throw new ArgumentException();
+            //return null;
+
+        }
+        public static string ConvertColor(char color)
+        {
+            return ConvertColor(color.ToString());
+        }
+        public static string ConvertCard(CollectCardOnHand cards)
+        {
+            return ConvertCard(cards.Card);
+        }
+        public static string ConvertCard(Card[] cards)
+        {
+            string result = "";
+            foreach (Card item in cards)
+            {
+                result += item.ToString() + " ";
+            }
+            return result;
+        }
+        public static string GetCard(string nameCommand, string command)
+        {
+            return command.Substring(nameCommand.Length);
+        }
+        public static string[] GetCards(string nameCommand, string command)
+        {
+            return command.Substring(nameCommand.Length).Split(' ');
+        }
+    }
+}
