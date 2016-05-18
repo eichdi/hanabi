957520e30fe086c1de2bafac3711b4894353955a
eichdi <eichdi96@gmail.com>
2016-05-18 15:33:58 +0300

    Add GetPlayer

--- hanabi/PlayerBase/PlayerBase.cs
+++ hanabi/PlayerBase/PlayerBase.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using hanabi.GameLogic;
 
 namespace hanabi.PlayerBase
 {
     class PlayerBase
     {
+        private List<Player> playerList;
+
+        public PlayerBase()
+        {
+            playerList = new List<Player>();
+        }
+        public Player GetPlayer(long id)
+        {
+            foreach (Player player in playerList)
+            {
+                if (id == player.ID)
+                {
+                    return player;
+                }
+            }
+            return new Player(id);
+        }
     }
 }
