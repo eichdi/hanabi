0bca04a58888dc0025dca1c3cb21e1d1df7340f1
eichdi <eichdi96@gmail.com>
2016-05-23 21:40:45 +0300

    Создан интерфейс для игры

---
+++ hanabi/GameLogic/PlayerGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace hanabi.GameLogic
+{
+    interface IPlayerGame
+    {
+        Player GetOpponent(Player player);
+        bool CheckPlayer(Player player);
+        Player CurrentPlayer { get; }
+        int GamedCards { get; }
+        CollectCardOnHand GetOpponentCard(Player player);
+        string GetTableCard();
+        Player NextPlayer { get; }
+        bool State { get; }
+    }
+}
